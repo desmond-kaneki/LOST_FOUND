e3e3d314185af951640ba96f04f6377af23f123f
desmond-kaneki <47731326+desmond-kaneki@users.noreply.github.com>
2019-11-08 01:40:03 +0530

    LOST AND FOUND PAGES FINAL

--- LOST_FOUND/LOST_FOUND/views.py
+++ LOST_FOUND/LOST_FOUND/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from item.models import item
 
 def lost(request):
-    it = item.objects
+    it = item.objects.order_by('-item_date')
     return render(request, 'lost.html',{'it':it})
 
 def found(request):
-    it = item.objects
+    it = item.objects.order_by('-item_date')
     return render(request, 'found.html',{'it':it})
