37dc52e98dbd2e1295f4c4388a62e5240d556898
desmond-kaneki <47731326+desmond-kaneki@users.noreply.github.com>
2019-11-26 10:29:54 +0530

    Final Touch Release_v1

--- LOST_FOUND/item/models.py
+++ LOST_FOUND/item/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class item(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items')
     item_name = models.CharField(max_length=128,null=False)
-    item_image = models.ImageField(upload_to='item_image/',blank=False,null=True)
+    item_image = models.ImageField(upload_to='item_image/',blank=True,null=True,default='/item_image/default.png')
     item_description = models.CharField(max_length=256,blank=False)
     item_date = models.DateField()
     item_status_choices = (('L','LOST'),('F','FOUND'))
